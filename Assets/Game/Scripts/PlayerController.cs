10edea16805b4f6521670ee27306e1f208b10559
Derman Can Danisman <dermancanwow@gmail.com>
2020-05-18 15:26:15 +0300

    Added player controller, scoring system

---
+++ Assets/Game/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int score = 0;
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        if (other.gameObject.tag == "Collectible")
+        {
+            Destroy(other.gameObject);
+            score++;
+        }
+
+        if (other.gameObject.tag == "FinishLine")
+        {
+            if (score == 4)
+            {
+                print("You have completed the level!");
+            }
+        }
+    }
+}
