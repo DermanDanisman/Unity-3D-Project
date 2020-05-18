689e9d629808d234eb1cd06daf8067156a157e13
Derman Can Danisman <dermancanwow@gmail.com>
2020-05-18 14:49:47 +0300

    Added firing mechanism every sec to a target

--- Assets/Game/Scripts/Launcher.cs
+++ Assets/Game/Scripts/Launcher.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class Launcher : MonoBehaviour
 {
     [SerializeField] GameObject projectile;
+    [SerializeField] GameObject target;
     [SerializeField] int launchVelocity = 1000;
+    float timer = 0;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        LaunchProjectile();
+    }
+
+    void LaunchProjectile()
+    {
+        timer += Time.deltaTime;
+
+        if(timer >= 1)
         {
             GameObject proj = Instantiate(projectile, transform.position, Quaternion.identity);
+            transform.LookAt(target.transform.position);
             proj.GetComponent<Rigidbody>().AddForce(transform.forward * launchVelocity);
-        }   
+            Destroy(proj, 3f);
+            timer = 0;
+        }
+
     }
 }
