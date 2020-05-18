6a2d5673c06a4771038134903db6679f72f1f58a
Derman Can Danisman <dermancanwow@gmail.com>
2020-05-18 14:36:45 +0300

    Added launcher script

---
+++ Assets/Game/Scripts/Launcher.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    [SerializeField] GameObject projectile;
+    [SerializeField] int launchVelocity = 1000;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            GameObject proj = Instantiate(projectile, transform.position, Quaternion.identity);
+            proj.GetComponent<Rigidbody>().AddForce(transform.forward * launchVelocity);
+        }   
+    }
+}
