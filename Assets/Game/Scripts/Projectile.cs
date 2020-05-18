7677dbccf2c79e2f8ffb4d5c324e51d5b6853e81
Derman Can Danisman <dermancanwow@gmail.com>
2020-05-18 15:00:14 +0300

    Added explosion effect to projectiles when collided with a target

---
+++ Assets/Game/Scripts/Projectile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] GameObject explosion;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Instantiate(explosion, transform.position, Quaternion.identity);
+        }
+    }
+}
