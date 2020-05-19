c5e0246fa63eced675ab057f1500f028c96bdb6b
Derman Can Danisman <dermancanwow@gmail.com>
2020-05-19 16:25:26 +0300

    Added raycast

---
+++ Assets/Game/Scripts/WeaponManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Ray rayFromPlayer;
+        rayFromPlayer = GetComponent<Camera>().ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        Debug.DrawRay(rayFromPlayer.origin, rayFromPlayer.direction * 10, Color.red);
+    }
+}
