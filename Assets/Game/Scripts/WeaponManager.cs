ad1c859ad64e3699f09b168a0891fe2d736145d6
Derman Can Danisman <dermancanwow@gmail.com>
2020-05-21 14:46:48 +0300

    Added shooting mechanism, effects and crosshair

--- Assets/Game/Scripts/WeaponManager.cs
+++ Assets/Game/Scripts/WeaponManager.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class WeaponManager : MonoBehaviour
 {
+    [SerializeField] GameObject shootingEffect;
     // Update is called once per frame
     void Update()
+    {
+        Shoot();
+    }
+
+    void Shoot()
     {
         Ray rayFromPlayer;
         rayFromPlayer = GetComponent<Camera>().ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
         Debug.DrawRay(rayFromPlayer.origin, rayFromPlayer.direction * 10, Color.red);
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(rayFromPlayer, out hit, 100))
+            {
+                print("You are looking at the " + hit.transform.gameObject.name);
+                Instantiate(shootingEffect, hit.point, Quaternion.identity);
+            }
+        }
     }
 }
