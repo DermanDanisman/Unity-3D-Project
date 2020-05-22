76909bd826c5e84757181eafa1c32fa3a077ac09
Derman Can Danisman <dermancanwow@gmail.com>
2020-05-22 19:02:54 +0300

    Added weapon switching mechanism

--- Assets/Game/Scripts/WeaponManager.cs
+++ Assets/Game/Scripts/WeaponManager.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class WeaponManager : MonoBehaviour
 {
     [SerializeField] GameObject shootingEffect;
+    
     int ammo;
-    // Update is called once per frame
+    float reloadTime;
+    bool startTimer;
+    bool canShoot = true;
+    string currentWeapon;
+    int weaponIndex;
+
+    [SerializeField] string[] weapons;
+    [SerializeField] int[] currentAmmo;
+    [SerializeField] int[] clipSize;
+    //[SerializeField] bool[] hasWeapon;
+
+    private void Start() 
+    {
+        weaponIndex = 0;
+        currentWeapon = weapons[weaponIndex];
+        print("Current weapon: " + currentWeapon);
+    }
     void Update()
     {
+        WeaponSwitch();
         Shoot();
     }
 
     void Shoot()
     {
         Ray rayFromPlayer = GetComponentInChildren<Camera>().ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+
         if (ammo > 0)
         {
             if (Input.GetKeyDown(KeyCode.F))
@@ -29,16 +48,42 @@ public class WeaponManager : MonoBehaviour
                     GameObject effect = Instantiate(shootingEffect, hit.point, Quaternion.identity);
                     Destroy(effect, 5);
                 }
-                if (hit.transform.name == "Target")
+                if (hit.transform.tag == "Target")
                 {
                     hit.transform.GetComponent<HealthManager>().GotHit(10);
                 }
                 else return;
+            }
+            if (ammo <= 0)
+            {
+                print("You need to reload!");
+            }
         }
-        if (ammo <= 0)
+    }
+
+    void WeaponSwitch()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
         {
-            print("You need to reload!");
-        }
+            if (currentWeapon == weapons[0])
+            {
+                weaponIndex++;
+                print(weaponIndex);
+                currentWeapon = weapons[weaponIndex];
+            }
+            else if (currentWeapon == weapons[1])
+            {
+                weaponIndex++;
+                print(weaponIndex);
+                currentWeapon = weapons[weaponIndex];
+            }
+            else if (currentWeapon == weapons[2])
+            {
+                weaponIndex = 0;
+                print(weaponIndex);
+                currentWeapon = weapons[weaponIndex];
+            }
+            print("Current weapon: " + currentWeapon + " | " + weaponIndex);
         }
     }
 
