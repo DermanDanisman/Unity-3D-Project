830b31e29cd861b9f3be1ba0e9febb20d69285d1
Derman Can Danisman <dermancanwow@gmail.com>
2020-06-06 21:09:44 +0300

    NPC can follow and attack player with animations

--- Assets/Game/First Person Part/Scripts/AnimatorController.cs
+++ Assets/Game/First Person Part/Scripts/AnimatorController.cs
@@ -10,44 +10,63 @@ public class AnimatorController : MonoBehaviour
     RaycastHit hit;
     Ray ray;
     GameObject objInFront;
+
+    [SerializeField] float rangeOfDistance;
     // Start is called before the first frame update
     void Start()
     {  
         animator = GetComponent<Animator>();
+        objInFront = gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
+        FollowPlayer();
+        AttackPlayer();
+    }
+
+    void FollowPlayer()
+    {
         //ray = new Ray();
-        ray.origin = transform.position + Vector3.up;
+        ray.origin = transform.position + Vector3.up * 1.5f;
         ray.direction = transform.forward * 10;
         Debug.DrawRay(ray.origin, ray.direction * 50, Color.red);
 
         if (Physics.Raycast(ray.origin, ray.direction * 100, out hit))
         {
             objInFront = hit.collider.gameObject;
-            print("Name: " + objInFront.name);
         }
 
-        if (objInFront.tag == "Player")
+        if (objInFront.gameObject.tag == "Player")
         {
+            gameObject.GetComponent<NavMeshAgent>().isStopped = false;
             animator.SetBool("canSeePlayer", true);
-            gameObject.GetComponent<NavMeshAgent>().SetDestination(GameObject.Find("FPSController").transform.position);
+            gameObject.GetComponent<NavMeshAgent>().SetDestination(objInFront.gameObject.transform.position);
+
+            Vector3 npcDirection = objInFront.gameObject.transform.position - transform.position;
+            npcDirection.y = 0;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(npcDirection), Time.time * 2);
+        }
+        else if (objInFront.gameObject.tag != "Player")
+        {
+            animator.SetBool("canSeePlayer", false);
+            gameObject.GetComponent<NavMeshAgent>().isStopped = true;
+        }
+
+       
+    }
+
+    void AttackPlayer()
+    {
+        rangeOfDistance = Vector3.Distance(GameObject.Find("FPSController").transform.position, transform.position);
+        if (rangeOfDistance < 2f)
+        {
+            animator.SetBool("withinRange", true);
         }
         else
         {
-            animator.SetBool("canSeePlayer", false);
-        }
-
-        if (info.IsName("Idle"))
-        {
-            //GetComponent<NavMeshAgent>().Stop();
-        }
-        if (info.IsName("Follow"))
-        {
-
-            //GetComponent<NavMeshAgent>().Resume();
+            animator.SetBool("withinRange", false);
         }
     }
 }
